ab3e651e6159c7c5aa9573f4ac08bf8eccba5495
Djimra NGARLEITA <dngarleita@gmail.com>
2024-09-24 03:53:21 +0300

    chore(#1): Use node redis client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,15 @@
+import { createClient } from "redis";
+
+let client;
+
+if (client === undefined) {
+  client = createClient();
+
+  client.on('connect', () => {
+    console.log('Redis client connected to the server');
+  });
+
+  client.on('error', (error) => {
+    console.log(`Redis client not connected to the server: ${error}`);
+  });
+}
